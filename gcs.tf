50d4c5e422346e95f5270a58f1e83382ca6050c1
RAJESH BOBBURI <43173667+bobburi@users.noreply.github.com>
2026-01-30 10:11:05 +0530

    added new gcs file

--- gcs.tf
+++ gcs.tf
@@ -5,10 +5,14 @@ uniform_bucket_level_access = true
 
 }
 
+
+
 resource "google_storage_bucket" "gcs-rest-1" {
 name = "gcs-rest-1209876-098"  
 location = "US"
 uniform_bucket_level_access = true
 
 }
+
+
 
